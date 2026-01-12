ba2a48f316948b348d67c812d860b3620d511a90
Felipe Salles Lopes <felipe28101997@gmail.com>
2026-01-12 16:55:22 -0400

    feat(item): add PurchaseItem entity

---
+++ src/main/java/com/raptor/groceries/PurchaseItem.java
@@ -0,0 +1,35 @@
+package com.raptor.groceries;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "purchase_items")
+@Entity
+public class PurchaseItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "purchase_id")
+    private Purchase purchase;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Enumerated(EnumType.STRING)
+    private UnitsMeasurement unitsMeasurement;
+
+    private BigDecimal quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+}
