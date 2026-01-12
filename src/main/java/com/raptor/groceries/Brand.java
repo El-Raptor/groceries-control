4ac666c16b4cd35068428cac476fe3f5dc9a6af5
Felipe Salles Lopes <felipe28101997@gmail.com>
2026-01-12 17:03:29 -0400

    feat(brand): implement Brand entity

---
+++ src/main/java/com/raptor/groceries/Brand.java
@@ -0,0 +1,20 @@
+package com.raptor.groceries;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "brands")
+@Entity
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "brand_id")
+    private long id;
+    @Column(name = "brand_name")
+    private String name;
+}
