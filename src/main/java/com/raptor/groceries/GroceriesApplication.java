f01be5de7d159227c76269889cb96be06b16df74
Felipe Salles Lopes <felipe28101997@gmail.com>
2026-02-24 15:15:12 -0400

    ops(core):  add database configuration

--- src/main/java/com/raptor/groceries/GroceriesApplication.java
+++ src/main/java/com/raptor/groceries/GroceriesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GroceriesApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(GroceriesApplication.class, args);
 	}
 
